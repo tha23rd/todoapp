4dfcccf9eb5d052a81f2b98162f7ae231e565d1d
tha23rd <>
2021-07-18 11:31:01 -0400

    adding rethinkdb and basic models

---
+++ api/app/models/todo_list.py
@@ -0,0 +1,23 @@
+import uuid
+from datetime import datetime
+from typing import List
+from typing import Optional
+
+from pydantic import BaseModel
+from rethinkdb import r
+
+
+class TodoItem(BaseModel):
+    id: str = uuid.uuid1().hex
+    created_date: datetime = r.now()
+    created_by: str  # user_id, for now could be optional?
+    content: str
+    is_complete: bool = False
+
+
+class TodoList(BaseModel):
+    id: Optional[str]
+    name: str
+    created_date: datetime = r.now()
+    updated_date: datetime = r.now()
+    items: List[TodoItem]
