b9bf177dc5f0e0a41658aced04dfa5e64dd408a9
tha23rd <>
2021-07-18 17:13:11 -0400

    adding commit files

--- ui/.prettierrc.js
+++ ui/.prettierrc.js
@@ -8,5 +8,6 @@ module.exports = {
   svelteStrictMode: false,
   svelteBracketNewLine: true,
   svelteIndentScriptAndStyle: true,
-  trailingComma: 'none'
+  trailingComma: 'none',
+  tabWidth: 4,
 }
