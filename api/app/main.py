fe8464418694b495fe2de95d9bbd8017fa2b5504
Zach Bluhm <54587508+tha23rd@users.noreply.github.com>
2021-07-18 10:43:41 -0400

    Create main.yml (#8)

--- api/app/main.py
+++ api/app/main.py
@@ -1,5 +1,6 @@
-from app.core.config import settings
 from fastapi import FastAPI
+
+from app.core.config import settings
 
 app = FastAPI(
     title=settings.PROJECT_NAME, openapi_url=f"{settings.API_V1_STR}/openapi.json"
