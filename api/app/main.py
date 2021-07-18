74868a78761a3ceb19afc4cb8fc90796314321b9
tha23rd <>
2021-07-18 10:16:30 -0400

    adding

--- api/app/main.py
+++ api/app/main.py
@@ -1,5 +1,9 @@
+import datetime
+
 from app.core.config import settings
 from fastapi import FastAPI
+
+print(datetime.datetime.now())
 
 app = FastAPI(
     title=settings.PROJECT_NAME, openapi_url=f"{settings.API_V1_STR}/openapi.json"
