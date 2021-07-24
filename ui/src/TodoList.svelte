2a98a27cb11b939cdcf696a3303b2aac31c43f95
tha23rd <>
2021-07-24 08:59:06 -0400

    implement delete and fade transition

--- ui/src/TodoList.svelte
+++ ui/src/TodoList.svelte
@@ -21,7 +21,7 @@
     import HeaderBar from './components/HeaderBar.svelte'
     import moment from 'moment-timezone'
     import { base_uri, ws_base_uri } from './constants'
-
+    import { fade } from 'svelte/transition'
     let new_item = ''
     let todolist = { items: [] }
     let list_name = 'My New List'
@@ -115,7 +115,14 @@
     }
     async function handleDelete(item) {
         const result = await fetch(`${base_uri}/todolist/${params.id}`, {
-            method: 'DELETE'
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                id: item.id,
+                path: 'delete_item'
+            })
         })
     }
 </script>
@@ -135,8 +142,8 @@
             />
         </div>
         <div class="flex flex-col">
-            {#each todolist.items as item}
-                <div class="flex">
+            {#each todolist.items as item (item.id)}
+                <div transition:fade={{ duration: 100 }} class="flex my-4">
                     <input
                         bind:checked={item.is_complete}
                         on:click={() => onCheck(item)}
@@ -167,8 +174,9 @@
                         class="text-2xl font-normal self-center">{item.name}</span
                     >
                     <button
+                        on:click={() => handleDelete(item)}
                         type="button"
-                        class="w-16 h-16 items-center bg-whiteish inline-flex ml-auto self-center border-none"
+                        class="w-8 h-8 items-center bg-whiteish inline-flex ml-auto self-center border-none"
                     >
                         <!-- <svg class="w-6 h-6 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="none" d="M15.584 10.001L13.998 8.417 5.903 16.512 5.374 18.626 7.488 18.097z"/><path d="M4.03,15.758l-1,4c-0.086,0.341,0.015,0.701,0.263,0.949C3.482,20.896,3.738,21,4,21c0.081,0,0.162-0.01,0.242-0.03l4-1 c0.176-0.044,0.337-0.135,0.465-0.263l8.292-8.292l1.294,1.292l1.414-1.414l-1.294-1.292L21,7.414 c0.378-0.378,0.586-0.88,0.586-1.414S21.378,4.964,21,4.586L19.414,3c-0.756-0.756-2.072-0.756-2.828,0l-2.589,2.589l-1.298-1.296 l-1.414,1.414l1.298,1.296l-8.29,8.29C4.165,15.421,4.074,15.582,4.03,15.758z M5.903,16.512l8.095-8.095l1.586,1.584 l-8.096,8.096l-2.114,0.529L5.903,16.512z"/></svg> -->
                         <!-- <svg
