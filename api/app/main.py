b93d5d6d2b75eb49f260740925993d7a995bec9c
tha23rd <>
2021-07-24 09:42:49 -0400

    cors

--- api/app/main.py
+++ api/app/main.py
@@ -20,7 +20,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins="*",
+    allow_origins=settings.BACKEND_CORS_ORIGINS,
     allow_methods=["*"],
     allow_headers=["*"],
 )
