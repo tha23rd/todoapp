5d741fc145db0ef845c9d78f82d950527185ca9a
tha23rd <>
2021-07-25 18:28:29 -0400

    implementing check

--- ui/src/TodoList.svelte
+++ ui/src/TodoList.svelte
@@ -66,7 +66,6 @@
     }
 
     function changeListName() {
-        console.log('Calling@@')
         fetch(`${base_uri}/todolist/${params.id}`, {
             method: 'PATCH',
             headers: {
@@ -94,7 +93,6 @@
             },
             body: JSON.stringify({ name: new_item })
         })
-        // console.log(res)
         console.log(`adding new item! ${new_item}`)
         new_item = ''
     }
@@ -115,15 +113,8 @@
         })
     }
     async function handleDelete(item) {
-        const result = await fetch(`${base_uri}/todolist/${params.id}`, {
-            method: 'DELETE',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                id: item.id,
-                path: 'delete_item'
-            })
+        await fetch(`${base_uri}/todolist/${params.id}/${item.id}`, {
+            method: 'DELETE'
         })
     }
 </script>
@@ -158,7 +149,8 @@
                         <svg
                             xmlns="http://www.w3.org/2000/svg"
                             class:text-greenish={item.is_complete}
-                            class="fill-current hidden pointer-events-none text-grayish"
+                            class:hidden={!item.is_complete}
+                            class="fill-current pointer-events-none text-grayish"
                             viewBox="0 0 512 512"
                             ><title>Checkmark</title><path
                                 fill="none"
@@ -179,26 +171,6 @@
                         type="button"
                         class="w-8 h-8 items-center bg-whiteish inline-flex ml-auto self-center border-none"
                     >
-                        <!-- <svg class="w-6 h-6 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="none" d="M15.584 10.001L13.998 8.417 5.903 16.512 5.374 18.626 7.488 18.097z"/><path d="M4.03,15.758l-1,4c-0.086,0.341,0.015,0.701,0.263,0.949C3.482,20.896,3.738,21,4,21c0.081,0,0.162-0.01,0.242-0.03l4-1 c0.176-0.044,0.337-0.135,0.465-0.263l8.292-8.292l1.294,1.292l1.414-1.414l-1.294-1.292L21,7.414 c0.378-0.378,0.586-0.88,0.586-1.414S21.378,4.964,21,4.586L19.414,3c-0.756-0.756-2.072-0.756-2.828,0l-2.589,2.589l-1.298-1.296 l-1.414,1.414l1.298,1.296l-8.29,8.29C4.165,15.421,4.074,15.582,4.03,15.758z M5.903,16.512l8.095-8.095l1.586,1.584 l-8.096,8.096l-2.114,0.529L5.903,16.512z"/></svg> -->
-                        <!-- <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            class="ionicon w-10 h-10 fill-current text-grayish ml-auto"
-                            viewBox="0 0 512 512"
-                            ><title>Close Circle</title><path
-                                d="M448 256c0-106-86-192-192-192S64 150 64 256s86 192 192 192 192-86 192-192z"
-                                fill="none"
-                                stroke="currentColor"
-                                stroke-miterlimit="10"
-                                stroke-width="32"
-                            /><path
-                                fill="none"
-                                stroke="currentColor"
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
-                                stroke-width="32"
-                                d="M320 320L192 192M192 320l128-128"
-                            /></svg
-                        > -->
                         <svg
                             class="w-8 h-8 ml-auto"
                             viewBox="0 0 27 27"
